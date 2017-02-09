ad65ec989995be0554ab6cc3d4f28eaf7189c6a7
AndersonCossul <anderson_cossul@hotmail.com>
2017-02-09 21:10:36 -0200

    vue-router set

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <my-header></my-header>
-    <home></home>
+    <div class="my-header">
+      <a href="/"><img src="./assets/layout/header-logo.png" alt="Space Science"></a>
+      <ul>
+        <a v-for="item in menu_items" v-bind:href="item.url" v-on:click.prevent="toggleMenu">
+          <li :class="{'active':active_menu == item.name}">{{item.name}}</li>
+        </a>
+      </ul>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import MyHeader from './components/MyHeader'
-import Home from './components/Home'
-import './assets/css/base.css'
-
 export default {
-  name: 'app',
-  components: {
-    MyHeader,
-    Home
+  name: 'my-header',
+  data () {
+    return {
+      active_menu: 'Home',
+      menu_items: [
+        {
+          name: 'Home',
+          url: '/'
+        },
+        {
+          name: 'About',
+          url: '/about'
+        },
+        {
+          name: 'Projects',
+          url: '/projects'
+        },
+        {
+          name: 'Blog',
+          url: '/blog'
+        },
+        {
+          name: 'Contact',
+          url: '/contact'
+        }
+      ]
+    }
+  },
+  methods: {
+    toggleMenu: function() {
+      console.log("don't know yet")
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import MyHeader from './components/MyHeader'
+import Home from './components/Home'
+import './assets/css/base.css'
 
-/* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', component: Home}
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App }
 })
